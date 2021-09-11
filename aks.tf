1b399b07e791836782b47decedc49675c80d4d1c
kodekolli <79526430+kodekolli@users.noreply.github.com>
2021-09-11 21:05:58 +0530

    Update aks.tf

--- aks.tf
+++ aks.tf
@@ -1,6 +1,9 @@
 provider "azurerm" {
-features {}
-    use_msi = false
+    client_id       =   var.client_id
+    client_secret   =   var.client_secret
+    subscription_id =   var.subscription_id
+    tenant_id       =   var.tenant_id
+    features {}
 }
 
 # Create a resource group if it doesn't exist
